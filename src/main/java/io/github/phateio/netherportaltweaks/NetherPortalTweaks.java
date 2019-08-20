398fbd3aac7fbb39a57533dc58ca7704805aa720
WhiteCat <whitecatx6@gmail.com>
2019-08-20 10:50:52 +0800

    First commit

---
+++ src/main/java/io/github/phateio/netherportaltweaks/NetherPortalTweaks.java
@@ -0,0 +1,20 @@
+package io.github.phateio.netherportaltweaks;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NetherPortalTweaks extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().setPortalCooldown(0);
+    }
+
+}
